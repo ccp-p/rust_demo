5116a16783729a47b72353b34077a1eae1c4f8d3
chenchengpeng <chenchengpeng@richinfo.cn>
2024-11-29 10:58:40 +0800

    添加命令行参数解析功能，支持从文件读取并逐行输出内容；新增.gitignore文件以忽略target目录

--- 02_cat/src/main.rs
+++ 02_cat/src/main.rs
@@ -1,3 +1,28 @@
+use  clap::Parser;
+
+#[derive(Parser)]
+struct Args{
+    files: Vec<String>
+    
+}
 fn main() {
     println!("Hello, world!");
+    if let Err(e) = run(Args::parse()){
+        eprintln!("Error: {}", e);
+        std::process::exit(1);
+    }
+   
+}
+
+fn run (args: Args) -> Result<(), Box<dyn std::error::Error>>{
+    let mut line_num = 1;
+
+    for file in args.files{
+        let content = std::fs::read_to_string(&file)?;
+        for line in content.lines(){
+            println!("{:4}: {}", line_num, line);
+            line_num += 1;
+        }
+    }
+    Ok(())
 }
